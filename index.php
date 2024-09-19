a56907fd784df31060811a3b3fb382910af46729
Michel Omena <micheltenoriodeomena@hotmail.com>
2024-09-19 21:24:14 +0100

    layout

--- index.php
+++ index.php
@@ -18,12 +18,11 @@ $location = [];
 $location['name'] = $data['location']['name'];
 $location['region'] = $data['location']['region'];
 $location['country'] = $data['location']['country'];
-$location['latitude'] = $data['location']['lat'];
-$location['longitude'] = $data['location']['lon'];
 $location['current_time'] = $data['location']['localtime'];
 
 //current weather data
 $current = [];
+$current['info'] = 'right now';
 $current['temperature'] = $data['current']['temp_c']; 
 $current['condition'] = $data['current']['condition']['text'];
 $current['condition_icon'] = $data['current']['condition']['icon'];
@@ -33,6 +32,7 @@ $current['wind_speed'] = $data['current']['wind_kph'];
 $forecast = [];
 foreach($data['forecast']['forecastday'] as $day){
     $forecast_day = [];
+    $forecast_day['info'] = null;
     $forecast_day['date'] = $day['date'];
     $forecast_day['condition'] = $day['day']['condition']['text'];
     $forecast_day['condition'] = $day['day']['condition']['icon'];
@@ -40,9 +40,33 @@ foreach($data['forecast']['forecastday'] as $day){
     $forecast_day['min_temp'] = $day['day']['mintemp_c'];
     $forecast[] = $forecast_day;
 }
+?>
 
-echo '<prep>';
-print_r($location);
-print_r($current);
-print_r($forecast);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">"
+</head>
+<body class="bg-dark text-white">
+    <div class="row justify-content-center mt-5">
+        <div class="col-10 p-5 bg-light text-black">
+            <h3>City Time <strong><?= $city ?></strong></h3>
+            <hr>
+
+            <!-- current -->
+             <?php 
+             $weather_info = $current;
+             include 'inc/weather_info.php';
+             ?>
+             <?php ?>
+             <?php ?>
+             <?php ?>
+        </div>
+
+    </div>
+</body>
+</html>
 
