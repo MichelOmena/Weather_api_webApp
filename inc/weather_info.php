4f0545fea2ff323b539d72cdb3e2ce2941351c5a
Michel Omena <micheltenoriodeomena@hotmail.com>
2024-09-19 21:43:03 +0100

    icon add

--- inc/weather_info.php
+++ inc/weather_info.php
@@ -2,4 +2,11 @@
     <div>
         <?= $weather_info['info'] ?>
     </div>
+
+    <div class="d-flex align-items-cneter">
+        <!-- icon -->
+         <div class="me-5">
+            <img src="<?php $weather_info['condition_icon'] ?>" class="img-fluid d-block">
+         </div>
+    </div>
 </div>
